a7ba728a4a278b1ba17f066f2408a270f39280e4
rpontodev <alvarobarbosa127@gmail.com>
2026-03-20 22:32:18 -0300

    primeiro commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+           Atividade simples com react+vite+routes
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <section className='container'>
+                <div className='apresentacao'>
+                    <p>Óla, sou <br />
+                        <span>Álvaro César</span> <br />
+                        Dev Frontend
+                    </p>
+                    <button className='btn btn-red'>
+                        Saiba mais
+                    </button>
+                </div>
+                <figure>
+                    <img className='img-home' src="/developer-red.svg" alt="Imagem de home" />
+                </figure>
+            </section>
+            <Footer />
+        </>
+    )
+}
+
+export default Home
