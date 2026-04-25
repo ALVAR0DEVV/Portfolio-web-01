9bf938ee74f286c736832b8eaf7819375d7a43e0
Alvaro Barbosa <alvarobarbosa127@gmail.com>
2026-04-24 22:46:39 -0300

    Atualização home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
@@ -11,9 +12,9 @@ function Home() {
                         <span>Álvaro César</span> <br />
                         Dev Frontend
                     </p>
-                    <button className='btn btn-red'>
+                    <Link to="/sobre" className='btn btn-red'>
                         Saiba mais
-                    </button>
+                    </Link>
                 </div>
                 <figure>
                     <img className='img-home' src="/developer-red.svg" alt="Imagem de home" />
